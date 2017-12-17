7f46f92c96c464708348660b129d05bb52a71d7c
Yasodharan_24 <Yasodharan24@github.com>
2017-12-18 02:22:19 +0530

    orderlater

--- application/views/Customer/OrderLater.php
+++ application/views/Customer/OrderLater.php
@@ -6,135 +6,81 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="header">
-                        <h4 class="title">Striped Table with Hover</h4>
-                        <p class="category">Here is a subtitle for this table</p>
+                        <h4 class="title">The Food Menu</h4>
+                        <p class="category">Most delicious and so many varities</p>
                     </div>
                     <div class="content table-responsive table-full-width">
                         <table class="table table-hover table-striped">
                             <thead>
-                            <th>ID</th>
                             <th>Name</th>
-                            <th>Salary</th>
-                            <th>Country</th>
-                            <th>City</th>
+                            <th>Unit price</th>
+                            <th>View</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Dakota Rice</td>
-                                <td>$36,738</td>
-                                <td>Niger</td>
-                                <td>Oud-Turnhout</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Minerva Hooper</td>
-                                <td>$23,789</td>
-                                <td>Curaçao</td>
-                                <td>Sinaai-Waas</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Sage Rodriguez</td>
-                                <td>$56,142</td>
-                                <td>Netherlands</td>
-                                <td>Baileux</td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>Philip Chaney</td>
-                                <td>$38,735</td>
-                                <td>Korea, South</td>
-                                <td>Overland Park</td>
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>Doris Greene</td>
-                                <td>$63,542</td>
-                                <td>Malawi</td>
-                                <td>Feldkirchen in Kärnten</td>
-                            </tr>
-                            <tr>
-                                <td>6</td>
-                                <td>Mason Porter</td>
-                                <td>$78,615</td>
-                                <td>Chile</td>
-                                <td>Gloucester</td>
-                            </tr>
+                              <?php
+                            /*  foreach ($food_itemses as $food_items) {
+                            echo '<tr>';
+                            echo '<td>'.$food_items->food_name.'</td>';
+                            echo '<td>'.$food_items->unit_price.'</td>';
+                            echo '</tr>';
+                          }*/
+                          ?>
+                          <tr>
+                            <td> Fish Bun </td>
+                            <td> 85 /= </td>
+                            <td> <form action="https://www.youtube.com/watch?v=HNZERFW8lYQ" method="post">
+                                <button type="submit" class="btn btn-block btn-success" > How we make it </button>
+                                </form> </td>
+                            <td>  </td>
+                            <td>  </td>
+                          </tr>
+                          <tr>
+                            <td> Pastry </td>
+                            <td> 65 /= </td>
+                            <td> <form action="https://www.youtube.com/watch?v=HNZERFW8lYQ" method="post">
+                                <button type="submit" class="btn btn-block btn-success" > How we make it </button>
+                                </form> </td>
+                            <td>  </td>
+                            <td>  </td>
+                          </tr>
+                          <tr>
+                            <td> Fish role </td>
+                            <td> 8o /= </td>
+                            <td> <form action="https://www.youtube.com/watch?v=HNZERFW8lYQ" method="post">
+                                <button type="submit" class="btn btn-block btn-success" > How we make it </button>
+                                </form> </td>
+                            <td>  </td>
+                            <td>  </td>
+                          </tr>
+                          <tr>
+                            <td> Egg Bun </td>
+                            <td> 100 /= </td>
+                            <td> <form action="https://www.youtube.com/watch?v=HNZERFW8lYQ" method="post">
+                                <button type="submit" class="btn btn-block btn-success" > How we make it </button>
+                                </form> </td>
+                            <td>  </td>
+                            <td>  </td>
+                          </tr>
+                          <tr>
+                            <td> Chicken devol </td>
+                            <td> 145 /= </td>
+                            <td> <form action="https://www.youtube.com/watch?v=HNZERFW8lYQ" method="post">
+                                <button type="submit" class="btn btn-block btn-success" > How we make it </button>
+                                </form> </td>
+                            <td>  </td>
+                            <td>  </td>
+                          </tr>
+
                             </tbody>
                         </table>
-
+                        <form action="https://www.youtube.com/watch?v=HNZERFW8lYQ" method="post">
+                            <button type="submit" class="btn btn-block btn-primary" > ORDER </button>
+                            </form>
                     </div>
                 </div>
             </div>
-
-
-            <div class="col-md-12">
-                <div class="card card-plain">
-                    <div class="header">
-                        <h4 class="title">Table on Plain Background</h4>
-                        <p class="category">Here is a subtitle for this table</p>
-                    </div>
-                    <div class="content table-responsive table-full-width">
-                        <table class="table table-hover">
-                            <thead>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Salary</th>
-                            <th>Country</th>
-                            <th>City</th>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Dakota Rice</td>
-                                <td>$36,738</td>
-                                <td>Niger</td>
-                                <td>Oud-Turnhout</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Minerva Hooper</td>
-                                <td>$23,789</td>
-                                <td>Curaçao</td>
-                                <td>Sinaai-Waas</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Sage Rodriguez</td>
-                                <td>$56,142</td>
-                                <td>Netherlands</td>
-                                <td>Baileux</td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>Philip Chaney</td>
-                                <td>$38,735</td>
-                                <td>Korea, South</td>
-                                <td>Overland Park</td>
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>Doris Greene</td>
-                                <td>$63,542</td>
-                                <td>Malawi</td>
-                                <td>Feldkirchen in Kärnten</td>
-                            </tr>
-                            <tr>
-                                <td>6</td>
-                                <td>Mason Porter</td>
-                                <td>$78,615</td>
-                                <td>Chile</td>
-                                <td>Gloucester</td>
-                            </tr>
-                            </tbody>
-                        </table>
-
-                    </div>
-                </div>
-            </div>
-
-
         </div>
     </div>
 </div>
