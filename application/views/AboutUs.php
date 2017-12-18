526d947602ca4d8c8e0074a1f6e1e64a46fd31a3
Heshan Harsha <heshan039@gmail.com>
2017-12-18 15:08:39 +0530

    AboutUsCompleate

--- application/views/AboutUs.php
+++ application/views/AboutUs.php
@@ -11,11 +11,42 @@
       <div class="container col-sm-9 col-sm-offset-0.5" align="center"  style="font-size: 20px; background-color: black ; border-radius: 10px; padding-top: 15px">
         Our online cafe is always ready to serve you. <br> Place  your order simply and wait until we deliver it to your doorstep..
         <br>
-        <br>Be Smart
-        <br>Save Time
-        <br>Save Money
+        <br>Be Smart.
+        <br>Save Time..
+        <br>Save Money...
       </div>
 <br>
+<br>
+	  <div class="container col-sm-10 col-sm-offset-0" align="left"  style="font-size: 18px; background-color: black ; border-radius: 10px; padding-top: 15px">
+	  	<div align="left">
+	  		<span style="color:#00FFFF">Address</span>
+	  		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  		No.28, Sri Gnaneshwara rd
+      		<br>
+      		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      		Dehiwala
+      		<br>
+      		<span style="color:#00FFFF">Fax</span>
+	  		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  		0212548798
+      		<br>
+      		<span style="color:#00FFFF">Tel.</span>
+	  		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  		0112548798
+      		<br>
+      		<span style="color:#00FFFF">e-mail</span>
+	  		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  		servemeonlinecafe@gmail.com
+      		<br>
+      		<br>
+      		<br>
+
+	  	</div>
+
+
+	  
+      
+      </div>
 
 	
 
